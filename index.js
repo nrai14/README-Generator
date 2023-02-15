8073fc876fdc99814c0263e79acb547e3535682a
nrai14 <nishadrai14@gmail.com>
2023-02-15 23:04:50 +0000

    pseudocoding + added more prompts and lists

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// basically import 
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
@@ -19,11 +20,26 @@ inquirer
         type: "input",
         name: "description",
         message: "What is the application about?"
-    }
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How do you install your application?"
+  },{
+        type: "input",
+        name: "usage",
+        message: "How do you use this application?"
+    },{
+      type: "list",
+      name: "license",
+      message: "What license does this application use?",
+      choices: ["MIT", "ISC", "zLib", "Apache"]
+  },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
-    let content = answers.title
+    let content = generateMarkdown(answers)
+
 
 
     fs.writeFile('./readme.txt', content, err => {
@@ -34,13 +50,13 @@ inquirer
 
     // currently returns answer as an object 
   })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+  // .catch((error) => {
+  //   if (error.isTtyError) {
+  //     // Prompt couldn't be rendered in the current environment
+  //   } else {
+  //     // Something else went wrong
+  //   }
+  // });
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -57,3 +73,22 @@ init();
 
 
 // let content = answers.title
+
+
+/* Has to include:
+
+- Title
+- Description 
+
+
+- Table of Contents
+- Installation
+- Usage
+- License (list of options then badge for license is added near the top of the READMe and a notice is added to the section of the README entitled license that explains which license the applicaiton is covered under)
+- When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
+- When a user enters their email address then this is added to the section of the README entitled Questions, with instructions on how to reach them with additional questions
+- When a user clicks on the links in the Table of Contents then they are taken to the corresponding section of the README
+
+*/
+
+// if statement function for basdge
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,80 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // return `# ${data.title}
+  
+  // ## ${data.description}
+  return `
+  # BingeBuddies
 
-`;
+
+## Description
+${data.description}
+
+## Usage
+${data.usage}
+
+## Screenshot
+
+![screenshot](assets/images/Screenshot1.png)
+![screenshot2](assets/images/Screenshot2.png)
+![screenshot3](assets/images/Screenshot3.png)
+![screenshot4](assets/images/Screenshot4.png)
+
+## Features
+
+Random Top pick of the day TV show
+Search according to the genres
+Favourites list
+Watch full trailer on full screen mode
+
+## Installation
+
+N/A 
+
+
+## Credits
+
+1. https://developers.google.com/youtube/v3 - With the YouTube Data API, you can add a variety of YouTube features to your application. Use the API to upload videos, manage playlists and subscriptions, update channel settings, and more.
+2. https://www.themoviedb.org/ -The API service is for those of you interested in using movie, TV show or actor images and/or data in your application. The API is a system we provide for you and your team to programmatically fetch and use our data and/or images.
+
+Github handles:
+1. [sanjana-1604](https://github.com/sanjana-1604)
+2. [sabrina-martorelli](https://github.com/sabrina-martorelli)
+3. [nrai14](https://github.com/nrai14) 
+4. [lauralp92](https://github.com/lauralp92)
+
+## License
+
+${generateBadge(data.license)}
+
+Copyright (c) 2022 Sabrina Martorelli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+## Deployed Link
+https://sabrina-martorelli.github.io/BingeBuddies/`
+  
+  
+  
+  ;
+}
+
+function generateBadge(license) {
+  if (license === 'MIT') {
+    return //badge string - how to display badge in markdown file 
+  }
+  else if (license === ' ') {
+    return // another badge string
+}
+  else {
+    return //final one 
+  }
 }
 
 module.exports = generateMarkdown;
